6a51358ed37034b28b5d8d468b97c08a89761622
DmitryOntov <Ontoev2016@gmail.com>
2023-10-11 16:58:12 +0800

    делаю верстку для iphone xr

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -19,12 +19,12 @@ const Header = ({isAuth, setIsAuth}) => {
       {
         isAuth ? 
         <nav  className={s.nav}>
-           <NavLink to="/auth" onClick={() => setIsAuth(!isAuth) & removeToken()} className={active => active.isActive? s.currentButton : s.button}>выход</NavLink>
-           <NavLink to="/deleted" className={active => active.isActive? s.currentButton : s.button}>Корзина</NavLink>
-           <NavLink to={"/main"} className={active => active.isActive? s.currentButton : s.button}>Разделы</NavLink>
-           <NavLink to="/zhurnal" className={active => active.isActive? s.currentButton : s.button}>Журнал</NavLink>
-           <NavLink to="/dead-line" className={active => active.isActive? s.currentButton : s.button}>Календарь</NavLink>
-           <NavLink to="/incoming" className={active => active.isActive? s.currentButton : s.button}>Входящие</NavLink>
+           <NavLink to="/auth" onClick={() => setIsAuth(!isAuth) & removeToken()} className={active => active.isActive? s.button : s.currentButton}>выход</NavLink>
+           <NavLink to="/deleted" className={active => active.isActive? s.button : s.currentButton}>Корзина</NavLink>
+           <NavLink to={"/main"} className={active => active.isActive? s.button : s.currentButton}>Разделы</NavLink>
+           <NavLink to="/zhurnal" className={active => active.isActive? s.button : s.currentButton}>Журнал</NavLink>
+           <NavLink to="/dead-line" className={active => active.isActive? s.button : s.currentButton}>Календарь</NavLink>
+           <NavLink to="/incoming" className={active => active.isActive? s.button : s.currentButton}>Входящие</NavLink>
         </nav>
         : 
         <NavLink to="/auth"> <button className={s.button}> авторизация</button></NavLink>

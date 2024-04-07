ecd20ddf62d68056a31521b9bd8e59602076c5ee
DmitryOntov <Ontoev2016@gmail.com>
2024-04-07 19:18:53 +0800

    делаю верстку для iphone xr

--- src/components/api/axios.api.js
+++ src/components/api/axios.api.js
@@ -1,18 +1,17 @@
 import axios from "axios";
 
-
 export const instance = axios.create({
-  baseURL: "https://e734-5-136-130-205.ngrok-free.app",
+  baseURL: "https://0ffa-5-136-130-205.ngrok-free.app",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
     Authorization: `Bearer ` + localStorage.getItem("token") || null,
     "ngrok-skip-browser-warning": "69420",
   },
-},);
+});
 
 instance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token'); // Получите токен из Local Storage (ваша логика может отличаться)
+    const token = localStorage.getItem("token"); // Получите токен из Local Storage (ваша логика может отличаться)
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`; // Добавьте токен в заголовок запроса
